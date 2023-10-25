34a2e9a1f77e1d4f1665daa6c0557a602a7ad4dd
flavio <104777358+FLAVIOSTAFIM@users.noreply.github.com>
2023-10-25 18:10:55 -0300

    eeee

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 function TocaSomPom(){
     document.querySelector('#som_tecla_pom').play();
     Document.querySelector('#som_tecla_puff').play();
+    Document.querySelector('#som_tecla_psh').play();
 }
 
 document.querySelector('.tecla_pom').onclick=TocaSomPom;
 document.querySelector('.tecla_puff').onclick=TocaSomPom;
+document.querySelector('tecla-psh').onclick=TocaSomPom;
 
 
 
