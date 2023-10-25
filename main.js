82624c258d510e55c619a70731defbf9907f41cf
flavio <104777358+FLAVIOSTAFIM@users.noreply.github.com>
2023-10-25 17:45:07 -0300

    gggg

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-document.querySelector('#som_tecla_pom').play();
-document.querySelector('.tecla_pom');
+function TocaSomPom{
+    document.querySelector('#som_tecla_pom');
+}
+
+document.querySelector('.tecla_pom').onclick=TocaSomPom
+;
+
+
 
